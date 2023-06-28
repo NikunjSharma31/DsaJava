eaa9b49270783cb9623cd5df76a1d65ac9a6ca85
Nikunj Sharma <sharmanicktop@gmail.com>
2023-06-28 16:44:24 +0530

    added multiply java

---
+++ MultiplyArray.java
@@ -0,0 +1,8 @@
+public class MultiplyArray {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        for(int j : arr){
+            System.out.println("The multiplied elemrent of array are : " + j*2);
+        }
+    }
+}
