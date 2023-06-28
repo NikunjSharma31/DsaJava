bc3ed24854425c686f2ba552206d3740a0142add
Nikunj Sharma <sharmanicktop@gmail.com>
2023-06-28 16:25:20 +0530

    changed in Array.java

--- Array.java
+++ Array.java
@@ -1,8 +1,8 @@
-class Array{
+class Array {
     public static void main(String[] args) {
-        int[] array = {1,2,3,4,5};
-        for(int j : array){
-            System.out.println(j);
+        int[] array = { 1, 2, 3, 4, 5 };
+        for (int j : array) {
+            System.out.println("The elements are : " + j);
         }
     }
 }
