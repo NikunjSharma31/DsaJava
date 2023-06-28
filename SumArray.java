82fde6db820386352a870438a07e2a09a642a8bb
Nikunj Sharma <sharmanicktop@gmail.com>
2023-06-28 17:13:21 +0530

    editted the SumArray.java file

--- SumArray.java
+++ SumArray.java
@@ -1,3 +1,9 @@
 public class SumArray {
-    
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5 };
+        for (int j : arr) {
+            j += j;
+            System.out.println(j);
+        }
+    }
 }
